82dbca98130535808293bd49c70b1a435a3c52de
pabloSB27 <pnsilva@alumnos.ubiobio.cl>
2016-10-25 12:19:45 -0300

    Se completa el codigo de index

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo "Hola Mundo";
-?>
+<doctype/>
+
+<html>
+<head>
+	<title>Mi pagina</title>
+</head>
+<body>
+	<h1><?php echo 'Hola Mundo'; ?></h1>
+	<?php
+		for ($i=2; $i < 6; $i++) { 
+			echo "<h$i>¿Cómo estás?</h$i>";
+		}
+		
+	?>
+</body>
+</html>
